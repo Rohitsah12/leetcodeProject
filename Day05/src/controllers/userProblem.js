93f44fe36e2bcb7b8a0697c2458fbbfbba63d2fd
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-05-23 16:51:33 +0530

    Created submission schemas and all problemcreator api

--- Day05/src/controllers/userProblem.js
+++ Day05/src/controllers/userProblem.js
@@ -49,4 +49,108 @@ const createProblem = async (req, res) => {
   }
 };
 
-module.exports = createProblem;
+const updateProblem= async (req,res)=>{
+  const {id}=req.params;
+  const {
+    title, description, difficulty, tags,
+    visibleTestCases, hiddenTestCases, startCode,
+    referenceSolution, problemCreator
+  } = req.body;
+  try {
+
+    if(!id){
+      return res.status(400).send("missing id field")
+    }
+
+    const DsaProblem=await Problem.findById(id);
+    if(!DsaProblem){
+      return res.status(404).send("Id is not present in server");
+    }
+    for (const { language, completeCode } of referenceSolution) {
+      const languageId = getLanguageById(language);
+
+      if (!languageId) {
+        return res.status(400).send(`Unsupported language: ${language}`);
+      }
+
+      const submissions = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input || "",
+        expected_output: testcase.output || ""
+      }));
+
+      console.log("Submissions Payload:", JSON.stringify(submissions, null, 2));
+
+      const submitResult = await submitBatch(submissions);
+      const resultTokens = submitResult.map((value) => value.token);
+      const testResults = await submitToken(resultTokens);
+
+      for (const test of testResults) {
+        if (test.status_id !== 3) {
+          return res.status(400).send("Error: One or more test cases failed.");
+        }
+      }
+    }
+
+    const newProblem=await Problem.findByIdAndUpdate(id,{...req.body},{runValidators:true,new:true});
+    res.status(200).send(newProblem);
+  } catch (error) {
+    
+    res.status(400).send("Error"+error);
+    
+  }
+
+}
+
+const deleteProblem=async (req,res) => {
+  const {id}=req.params;
+  try {
+    if(!id){
+      return res.status(400).send("missing id field")
+    }
+
+    const deletedProblem=await Problem.findByIdAndDelete(id);
+    if(!deleteProblem){
+      return res.status(404).send("Problem is missing");
+    }
+
+    res.status(200).send("Successfully deleted");
+  } catch (error) {
+    res.status(500).send("Error: "+error)
+  }
+}
+
+const getProblemById=async (req,res)=>{
+  const {id}=req.params
+  try {
+    if(!id){
+      return res.status(400).send("missing id field")
+    }
+
+    const getProblem=await Problem.findByIdAndDelete(id);
+    if(!getProblem){
+      return res.status(404).send("Problem is missing");
+    }
+
+    res.status(200).send(getProblem);
+  } catch (error) {
+     res.status(500).send("Error: "+error);
+  }
+}
+
+const getAllProblem=async (req,res)=>{
+
+  try {
+    const getProblem=await Problem.find({});
+    if(getProblem.length==0){
+      return res.status(404).send("Problem is missing");
+    }
+
+    res.status(200).send(getProblem);
+  } catch (error) {
+     res.status(500).send("Error: "+error);
+  }
+}
+
+module.exports ={createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem};
