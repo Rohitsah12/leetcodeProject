6d9f8bafb1edec783121263ffde8daf3de16affb
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-05-31 02:57:31 +0530

    Added problemPage on route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,50 @@
-import { Routes,Route, Navigate } from "react-router"
-import HomePage from "./pages/Homepage"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import { checkAuth } from "./authSlice"
-import { useDispatch,useSelector } from "react-redux"
-import { useEffect } from "react"
-import AdminPanel from "./pages/AdminPanel"
-import CodeEditor from "./pages/CodeEditor"
+import {Routes, Route ,Navigate} from "react-router";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Homepage from "./pages/Homepage";
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuth } from "./authSlice";
+import { useEffect } from "react";
+import AdminPanel from "./pages/AdminPanel";
+import ProblemPage from "./pages/ProblemPage";
 
-function App() {
+function App(){
+  
+  const dispatch = useDispatch();
+  const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
 
-  //code likhna pdega isAuthenticated or not
-  const {isAuthenticated,loading,user}=useSelector((state)=>state.auth);
-  const dispatch=useDispatch();
-  useEffect(()=>{
+  // check initial authentication
+  useEffect(() => {
     dispatch(checkAuth());
-  },[dispatch])
+  }, [dispatch]);
 
+  console.log(user)
+  console.log(isAuthenticated);
+  
   if (loading) {
     return <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
     </div>;
   }
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={isAuthenticated?<HomePage></HomePage>:<Navigate to='/signup'/>}></Route>
-        <Route path="/login" element={isAuthenticated?<Navigate to='/'/>:<Login></Login>}></Route>
-        <Route path="/signup" element={isAuthenticated?<Navigate to='/'/>:<Signup></Signup>}></Route>
-        <Route path="/admin" element={isAuthenticated && user?.role==='admin'? <AdminPanel></AdminPanel> :<Navigate to='/'></Navigate>}></Route>
-        <Route path="/code" element={<CodeEditor />}></Route>
-      </Routes>
-    </>
+  return(
+  <>
+    <Routes>
+      <Route path="/" element={isAuthenticated ?<Homepage></Homepage>:<Navigate to="/signup" />}></Route>
+      <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
+      <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
+      <Route 
+        path="/admin" 
+        element={
+          isAuthenticated && user?.role === 'admin' ? 
+            <AdminPanel /> : 
+            <Navigate to="/" />
+        } 
+      ></Route>
+      <Route path="/problem/:problemId" element={<ProblemPage></ProblemPage>}></Route>
+    </Routes>
+  </>
   )
 }
 
-export default App
+export default App;
