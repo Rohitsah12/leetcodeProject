be4dccef3217192ad17eb4cec6fa5df1be20d481
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-06-18 01:14:56 +0530

    fix: Correct syntax by adding missing semicolon and import statement in AiChatting route

--- Backend/src/routes/AiChatting.js
+++ Backend/src/routes/AiChatting.js
@@ -2,7 +2,8 @@ const express=require('express');
 
 const aiRouter=express.Router();
 
-const userMiddleWare=require('../middleware/userMiddleWare')
+const userMiddleWare=require('../middleware/userMiddleWare');
+const solveDoubt = require('../controllers/solveDoubt');
 aiRouter.post('/chat',userMiddleWare,solveDoubt);
 
 module.exports=aiRouter;
