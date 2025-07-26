c8aa8bf17d38ca3e00dfcebb82d92c79caab429f
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-07-26 22:14:05 +0530

    FIXED api issue

--- frontend/src/utils/axiosClient.jsx
+++ frontend/src/utils/axiosClient.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";   
+const apiUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";   
 
 const axiosClient=axios.create({
     baseURL:`${apiUrl}`,
