db096d9cac207a72542960fba7757b4c30840f9a
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-07-27 02:29:44 +0530

    redirect to home page

--- Backend/src/controllers/userAuthent.js
+++ Backend/src/controllers/userAuthent.js
@@ -132,7 +132,7 @@ res.cookie('token', token, getCookieOptions());
     req.logout((err) => {
       if (err) console.error('Session logout error:', err);
       
-      res.redirect(`${process.env.FRONTEND_URL}/auth-success?redirect=`);
+      res.redirect(`${process.env.FRONTEND_URL}`);
 
     });
 
